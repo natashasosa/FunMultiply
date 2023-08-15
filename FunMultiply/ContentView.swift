749241324e743b6d4108d99ff0513aedf1eb8285
Natasha Sosa Rebelo <natasha.sosa@poatek.com>
2023-08-15 16:23:54 -0300

    Start adding design elemments and colors to ContentView

--- FunMultiply/ContentView.swift
+++ FunMultiply/ContentView.swift
@@ -9,6 +9,12 @@ import SwiftUI
 
 struct ContentView: View {
     
+    init() {
+        UISegmentedControl.appearance().selectedSegmentTintColor = .purple
+        UISegmentedControl.appearance().setTitleTextAttributes([.foregroundColor : UIColor.white], for: .normal)
+    }
+    
+    
     @State private var tableRange = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
     @State private var table = 2
     @State private var questionRange = [5, 10, 20]
@@ -16,33 +22,72 @@ struct ContentView: View {
  
     var body: some View {
         NavigationView {
+            ZStack {
             
-            Form {
-                Section {
-                    Text("What table do you want to practice?")
-                        .font(.headline)
-                    Picker("", selection: $table) {
-                        ForEach(tableRange, id: \.self) {
-                            Text("\($0)")
+                VStack(spacing: 30) {
+                    Text("Fun Multiply")
+                        .font(.largeTitle.weight(.black))
+                        .foregroundColor(Color(red: 0.98, green: 0.67, blue: 0.09))
+                    
+                    VStack(spacing: 15) {
+                        Spacer()
+                        
+                        Section {
+                            Text("What table do you want to practice?")
+                                .font(.title3.weight(.semibold))
+                                .foregroundColor(.white)
+                            
+                            Picker("", selection: $table) {
+                                ForEach(tableRange, id: \.self) {
+                                    Text("\($0)")
+                                }
+                            }
+                            .pickerStyle(.segmented)
                         }
-                    }
-                    .pickerStyle(.segmented)
-                }
-                
-                Section {
-                    Text("How many questions do you want to answer?")
-                        .font(.headline)
-                    Picker("", selection: $numberOfQuestions) {
-                        ForEach(questionRange, id: \.self) {
-                            Text("\($0) questions")
+                        
+                        Spacer()
+                        
+                        Section {
+                            Text("How many questions do you want to answer?")
+                                .font(.title3.weight(.bold))
+                                .foregroundColor(.white)
+                            
+                            Picker("", selection: $numberOfQuestions) {
+                                ForEach(questionRange, id: \.self) {
+                                    Text("\($0) questions")
+                                }
+                            }
+                            .pickerStyle(.segmented)
                         }
+                        
+                        Spacer()
                     }
-                    .pickerStyle(.segmented)
+                    .frame(width: .infinity, height: 300)
+                    .padding(.vertical, 60)
+                    .padding(.horizontal, 10)
+                    .background(RadialGradient(stops: [
+                        .init(color: Color(red: 0.98, green: 0.67, blue: 0.09), location: 0.0),
+                        .init(color: Color(red: 0.75, green: 0.09, blue: 0.98), location: 0.6)
+                    ], center: .top, startRadius: 100, endRadius: 700))
+                    .clipShape(RoundedRectangle(cornerRadius: 30))
+                    
+                    Spacer()
+                    
+                    NavigationLink("Start", destination: GameView(table: table,numberOfQuestions: numberOfQuestions))
+                        .padding(.vertical, 10)
+                        .padding(.horizontal, 35)
+                        .background(Color(red: 0.98, green: 0.67, blue: 0.09))
+                        .clipShape(RoundedRectangle(cornerRadius: 10))
+                        .foregroundColor(.white)
+                        .font(.title3 .weight(.bold))
+                    
+                    Spacer()
+                        
                 }
-                
-                NavigationLink("Start", destination: GameView(table: table,numberOfQuestions: numberOfQuestions))
+                .padding()
             }
-            .navigationTitle("Fun multiply")
+            
+
         }
     }
 }
